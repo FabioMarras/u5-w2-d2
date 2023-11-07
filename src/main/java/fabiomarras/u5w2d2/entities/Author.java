aa4e4a581ea4d55e9d9d6dcb63742211a661284e
FabioMarras <fabio.marras@yahoo.com>
2023-11-07 15:29:56 +0100

    add Author

---
+++ src/main/java/fabiomarras/u5w2d2/entities/Author.java
@@ -0,0 +1,60 @@
+package fabiomarras.u5w2d2.entities;
+
+import java.util.Date;
+
+public class Author {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private Date dataDiNascita;
+    private String avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataDiNascita() {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(Date dataDiNascita) {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
